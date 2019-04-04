37b3ce2b7df3ecb1793f663436ca72a73487b3a9
Joseph Wolensky <josephwolensky@gmail.com>
2019-04-04 21:43:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,11 @@
-function basicTeenager(age) {
+function basicTeenager(13-19) {
+  switch (age) {
+    case "you are a teenager!":
+      return "You are a teenager!"
+      break;
+    default:
 
+  }
 }
 
 function teenager(age) {
